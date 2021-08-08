ef741fe01c3e0cf13f50b8cc9ca1bd4fde9a91f5
aosante <aosantea@ucenfotec.ac.cr>
2021-08-08 00:32:31 -0600

    add api and start using useQuery hook in components

--- client/src/BooksList/BooksList.jsx
+++ client/src/BooksList/BooksList.jsx
@@ -1,3 +1,38 @@
-const BooksList = () => <div>BooksList</div>;
+import { useQuery } from 'react-query';
+import { Flex } from 'rebass/styled-components';
+import Loader from 'react-loader-spinner';
+
+import { Container } from '../layout';
+import getAllBooks from '../api';
+
+const BooksList = () => {
+  const { data, error, isLoading, isError } = useQuery('books', getAllBooks);
+
+  if (isLoading) {
+    return (
+      <Container>
+        <Flex py="5" justifyContent="center">
+          <Loader type="ThreeDots" color="#ccc" height={30} />
+        </Flex>
+      </Container>
+    );
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <Container>
+      <Flex flexDirection="column" alignItems="center">
+        {data.map(({ author, title, id }) => (
+          <div key={id}>
+            {author} - {title}
+          </div>
+        ))}
+      </Flex>
+    </Container>
+  );
+};
 
 export default BooksList;
